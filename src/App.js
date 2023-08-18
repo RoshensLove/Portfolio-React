ee5a2906052e03caff5adaacf6f7bd5264d39e60
RoshensLove <alegatorwork@gmail.com>
2023-08-18 19:57:25 +0300

    8

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ScrollToTop from './utils/scrollToTop';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/Portfolio-React">
+      <Router basename="/Portfolio-React">
           <ScrollToTop/>
           <Navbar/>
           <Routes>
@@ -23,7 +23,7 @@ function App() {
             <Route path='/contacts' element={<Contacts/>}/>
           </Routes>
           <Footer/>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
