2703bf8f4bc56a50074d77936cb650bd64cedf43
RoshensLove <alegatorwork@gmail.com>
2023-08-21 19:54:05 +0300

    rename link to portfolio pdf in Header.js

--- src/conponents/header/Header.js
+++ src/conponents/header/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
               a frontend developer
               </h1>
               <p className="header__text">better than yesterday</p>
-              <a href="https://drive.google.com/file/d/1uCHmwotK5-XlXVNQEmfD9M_SatVMCZKv/view" target="_blank" className="btn">Download CV</a>
+              <a href="https://drive.google.com/file/d/1w7lf41NuEv-fTAVE5aAW3G2ZWBZfZGwF/view" target="_blank" className="btn">Download CV</a>
               
           </div>
       </header>
